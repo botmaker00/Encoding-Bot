d3ce06b286f1e43c5906e594f653b67eec64894d
Lokesh <23ca1031@mvn.edu.in>
2025-11-25 00:12:15 +0530

    Update __main__.py

--- VideoEncoder/__main__.py
+++ VideoEncoder/__main__.py
@@ -2,6 +2,18 @@ import dns.resolver
 from pyrogram import idle
 
 from . import app, log
+from pyrogram import utils
+
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+
+utils.get_peer_type = get_peer_type_new
 
 dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers = [
